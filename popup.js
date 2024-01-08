fe2fcce0cbbe9086c246a35cb93d171cd7a89d46
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-08 16:06:02 +0100

    Test Extension recherche Google / Wikipédia

---
+++ popup.js
@@ -0,0 +1,15 @@
+document.getElementById("googleSearch").addEventListener("click", function () {
+    const query = document.getElementById("searchBox").value;
+    if (query) {
+        const googleSearchURL = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+        chrome.tabs.create({ url: googleSearchURL });
+    }
+});
+
+document.getElementById("wikipediaSearch").addEventListener("click", function () {
+    const query = document.getElementById("searchBox").value;
+    if (query) {
+        const wikipediaSearchURL = `https://en.wikipedia.org/wiki/Special:Search?search=${encodeURIComponent(query)}`;
+        chrome.tabs.create({ url: wikipediaSearchURL });
+    }
+});
