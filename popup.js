9b8099bb1ca87ac88bebf9c3ac6ff7ab4f5abd31
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-09 14:24:01 +0100

    Update popup.html and popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,19 @@
-fetch(`https://drop-api.ea.com/rating/fc-24?locale=en&offset=0&limit=100&${id}`)
-    .then((response) => response.json())
-    .then((data)) => {
-        const 
-    }
+const fetchChelseaPlayers = () => {
+    fetch('https://drop-api.ea.com/rating/fc-24?locale=en&offset=0&limit=100&team=5')
+        .then(response => response.json())
+        .then(chelseaPlayersData => {
+            // Traitez les données des joueurs de Chelsea ici
+            console.log(chelseaPlayersData);
+
+            // Exemple : Afficher le nom des joueurs de Chelsea
+            chelseaPlayersData.forEach(player => {
+                console.log(player.firstName, player.lastName);
+            });
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la récupération des joueurs de Chelsea : ', error);
+        });
+};
+
+// Appelez la fonction pour récupérer les joueurs de Chelsea
+fetchChelseaPlayers();
