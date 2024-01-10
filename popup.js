8c82c6d3904fdb8a56d7f3ea82d363bb224c9a2c
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-10 14:28:57 +0100

    Update player details display in popup.html and add search functionality in popup.js

--- popup.js
+++ popup.js
@@ -1,18 +1,22 @@
-const fetchChelseaPlayers = () => {
+const fetchChelseaPlayers = (playerName) => {
     fetch('https://drop-api.ea.com/rating/fc-24?locale=en&offset=0&limit=100&team=5')
         .then(response => response.json())
         .then(chelseaPlayersData => {
-            // Traitez les données des joueurs de Chelsea ici
-            console.log(chelseaPlayersData);
+            const player = chelseaPlayersData.find(player => `${player.firstName} ${player.lastName}`.toLowerCase() === playerName.toLowerCase());
 
-            // Exemple : Afficher le nom des joueurs de Chelsea
-            chelseaPlayersData.forEach(player => {
-                console.log(player.firstName, player.lastName);
-            });
+            if (player) {
+                document.getElementById('firstName').textContent = player.firstName;
+                document.getElementById('lastName').textContent = player.lastName;
+            } else {
+                console.error('Joueur non trouvé');
+            }
         })
         .catch(error => {
             console.error('Une erreur s\'est produite lors de la récupération des joueurs de Chelsea : ', error);
         });
 };
 
-fetchChelseaPlayers();
+document.getElementById('search').addEventListener('click', () => {
+    const playerName = document.getElementById('playerName').value;
+    fetchChelseaPlayers(playerName);
+});
