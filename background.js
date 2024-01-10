dddf3c8e327813fc227d6bf0445905ff901d3573
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-10 16:20:34 +0100

    Refactor background.js and change.css, update change.html and manifest.json

--- background.js
+++ background.js
@@ -1,22 +1,26 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('Extension installed');
-  });
-  
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'fetchPlayerDetails') {
-      const playerName = message.playerName;
-      fetchPlayerDetails(playerName)
-        .then(playerData => {
-          chrome.tabs.sendMessage(sender.tab.id, { action: 'playerDetails', playerData });
-        })
-        .catch(error => {
-          console.error(`Error fetching player details: ${error}`);
+  console.log("Extension installed");
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "fetchPlayerDetails") {
+    const playerName = message.playerName;
+    fetchPlayerDetails(playerName)
+      .then((playerData) => {
+        chrome.tabs.sendMessage(sender.tab.id, {
+          action: "playerDetails",
+          playerData,
         });
-    }
-  });
-  
-  const fetchPlayerDetails = async playerName => {
-    const response = await fetch(`https://drop-api.ea.com/rating/fc-24?locale=en&offset=0&limit=100&firstName=${playerName}`);
-    return response.json();
-  };
-  
+      })
+      .catch((error) => {
+        console.error(`Error fetching player details: ${error}`);
+      });
+  }
+});
+
+const fetchPlayerDetails = async (playerName) => {
+  const response = await fetch(
+    `https://drop-api.ea.com/rating/fc-24?locale=en&offset=0&limit=100&firstName=${playerName}`
+  );
+  return response.json();
+};
