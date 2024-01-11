b58101c60fd414831b157a8b40605f9abece643a
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-11 15:32:53 +0100

    Refactor Money function to use async/await and handle errors

--- change.js
+++ change.js
@@ -1,6 +1,14 @@
-function Money (monnaie1, monnaie2){
-    let value = fetch(`https://v6.exchangerate-api.com/v6/47eafb64232b369043e6c27c/pair/${monnaie1}/${monnaie2}`)
-    console.log(value)
-}
-
-console.log(Money("EUR", "USD"))
+async function Money(monnaie1, monnaie2) {
+    try {
+      let response = await fetch(
+        `https://v6.exchangerate-api.com/v6/47eafb64232b369043e6c27c/pair/${monnaie1}/${monnaie2}`
+      );
+      let data = await response.json();
+      let transaction = parseFloat(data.conversion_rate);
+      let caculator = valeur * transaction;
+      console.log(caculator)
+    } catch (error) {
+      console.error("Une erreur s'est produite :", error);
+    }
+  }
+  Money("JPY", "EUR");
