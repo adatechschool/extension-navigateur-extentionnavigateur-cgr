51668af0aeecf64d7ba7fd826a71e7939e9650f1
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-11 15:34:16 +0100

    Refactor fetchPlayerDetails function and fix formatting in change.js

--- change.js
+++ change.js
@@ -1,14 +1,14 @@
 async function Money(monnaie1, monnaie2) {
     try {
-      let response = await fetch(
-        `https://v6.exchangerate-api.com/v6/47eafb64232b369043e6c27c/pair/${monnaie1}/${monnaie2}`
-      );
-      let data = await response.json();
-      let transaction = parseFloat(data.conversion_rate);
-      let caculator = valeur * transaction;
-      console.log(caculator)
+        let response = await fetch(
+            `https://v6.exchangerate-api.com/v6/47eafb64232b369043e6c27c/pair/${monnaie1}/${monnaie2}`
+        );
+        let data = await response.json();
+        let transaction = parseFloat(data.conversion_rate);
+        let caculator = valeur * transaction;
+        console.log(caculator)
     } catch (error) {
-      console.error("Une erreur s'est produite :", error);
+        console.error("Une erreur s'est produite : ", error);
     }
-  }
-  Money("JPY", "EUR");
+}
+Money("JPY", "EUR");
