76f7291ee311a63026f60c8fc2edae48829f162c
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-11 11:51:28 +0100

    Add currency conversion functionality

---
+++ change.js
@@ -0,0 +1,8 @@
+
+
+function Money (monnaie1, monnaie2){
+    let value = fetch(`https://v6.exchangerate-api.com/v6/47eafb64232b369043e6c27c/pair/${monnaie1}/${monnaie2}`)
+    console.log(value)
+}
+
+console.log(Money("EUR", "USD"))
