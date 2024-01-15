df2ac576ff785852c8c99c25e8092c8ce0f0bf05
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-15 10:35:52 +0100

    Update currency conversion functionality

--- change.js
+++ change.js
@@ -1,16 +1,20 @@
-let valeur = 100;
-
-async function Money(monnaie1, monnaie2) {
+async function money(monnaie1, monnaie2) {
+    let valeur0 = document.querySelector("#Value").value
+    console.log(valeur0);
     try {
         let response = await fetch(
             `https://v6.exchangerate-api.com/v6/47eafb64232b369043e6c27c/pair/${monnaie1}/${monnaie2}`
         );
         let data = await response.json();
         let transaction = parseFloat(data.conversion_rate);
-        let calculator = valeur * transaction;
-        console.log(calculator)
+        console.log(valeur0);
+        let valeur = parseFloat(valeur0);
+        let calculator = parseFloat(valeur * transaction);
+        console.log(calculator);
+        document.getElementById("final").value = calculator
     } catch (error) {
         console.error("Une erreur s'est produite : ", error);
     }
 }
-Money("JPY", "EUR");
+function money_choice() {
+}
