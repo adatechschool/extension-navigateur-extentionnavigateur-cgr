17980a12405386ee768d0f7efdd6e6b55c066075
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-16 14:42:58 +0100

    Update box-shadow color in change.css and add dynamic dropdown content in change.html and change.js

--- change.js
+++ change.js
@@ -22,9 +22,27 @@ async function money_choice() {
         fullJSON = await res.json();
         moneyConv = fullJSON["conversion_rates"]
         dico = Object.keys(moneyConv)
+        return (dico)
     } catch (error) {
         console.error("Une erreur s'est produite : ", error);
     }
 }
-
-money_choice()
+async function testasync() {
+    try {
+        résu = await money_choice()
+        //console.log("je suis le resultat", résu)
+        for (i = 0; i < résu.length; i++) {
+            console.log("je suis dans la boucle")
+            let drp = document.getElementById("dr");
+            let divElement = document.createElement("div");
+            divElement.innerHTML = `
+            <input type="radio" id="${résu[i]}" name="drone" value="${résu[i]}" checked />
+            <label for="${résu[i]}">${résu[i]}</label>
+        `;
+            document.getElementById("dr").appendChild(divElement);
+        }
+    } catch (error) {
+        console.error("Une erreur s'est produite : ", error);
+    }
+}
+testasync()
