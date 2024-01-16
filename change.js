da64cd341919ce0663773451e91ad85f11b1599c
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2024-01-16 15:19:58 +0100

    Refactor font-family and background gradient in change.css, and rename sendMoney function to money in change.js

--- change.js
+++ change.js
@@ -1,4 +1,4 @@
-async function sendMoney(monnaie1, monnaie2) {
+async function money(monnaie1, monnaie2) {
     let valeur0 = document.querySelector("#value1").value
     console.log(valeur0);
     try {
@@ -7,7 +7,6 @@ async function sendMoney(monnaie1, monnaie2) {
         );
         let data = await response.json();
         let transaction = parseFloat(data.conversion_rate);
-        console.log(valeur0);
         let valeur = parseFloat(valeur0);
         let calculator = parseFloat(valeur * transaction);
         console.log(calculator);
@@ -22,6 +21,7 @@ async function money_choice() {
         fullJSON = await res.json();
         moneyConv = fullJSON["conversion_rates"]
         dico = Object.keys(moneyConv)
+        console.log(dico)
         return (dico)
     } catch (error) {
         console.error("Une erreur s'est produite : ", error);
@@ -34,12 +34,19 @@ async function testasync() {
         for (i = 0; i < résu.length; i++) {
             console.log("je suis dans la boucle")
             let drp = document.getElementById("dr");
+            let drp1 = document.getElementById("dr1");
             let divElement = document.createElement("div");
+            let divElement1 = document.createElement("div1");
             divElement.innerHTML = `
             <input type="radio" id="${résu[i]}" name="drone" value="${résu[i]}" checked />
             <label for="${résu[i]}">${résu[i]}</label>
         `;
+            divElement1.innerHTML = `
+            <input type="radio" id="${résu[i]}" name="drone" value="${résu[i]}" checked />
+            <label for="${résu[i]}">${résu[i]}</label>
+        `;
             document.getElementById("dr").appendChild(divElement);
+            document.getElementById("dr1").appendChild(divElement1);
         }
     } catch (error) {
         console.error("Une erreur s'est produite : ", error);
